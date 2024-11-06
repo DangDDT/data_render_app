54a2582a963d554d4509b880004aa7fbd3cbd28a
dangddt <jacob15072000@gmail.com>
2024-11-06 22:13:24 +0700

    Adjust font size and position in MovingLines component

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -227,7 +227,7 @@ const Line = React.memo(() => {
 const LineWithCircleLeftUp = React.memo(() => {
   return (
     <div className="flex">
-      <div className="absolute left-[-30px] top-[2px] h-4 w-4 rounded-full border-4 border-[#4ffc92]"></div>
+      <div className="absolute left-[-30px] top-[-2px] h-4 w-4 rounded-full border-4 border-[#4ffc92]"></div>
       <div className="flex">
         <div className="absolute left-[-19px] mb-0 mt-0 h-0.5 w-6 rotate-45 transform rounded-md bg-[#4ffc92]"></div>
         <div className="mb-2 mt-2 h-0.5 w-16 rounded-md bg-[#4ffc92]"></div>
@@ -239,7 +239,7 @@ const LineWithCircleLeftUp = React.memo(() => {
 const LineWithCircleLeftDown = React.memo(() => {
   return (
     <div className="flex">
-      <div className="absolute bottom-[4px] left-[-30px] h-4 w-4 rounded-full border-4 border-[#4ffc92]"></div>
+      <div className="absolute bottom-[-2px] left-[-30px] h-4 w-4 rounded-full border-4 border-[#4ffc92]"></div>
       <div className="flex">
         <div className="absolute left-[-20px] top-[37px] mb-0 mt-0 h-0.5 w-6 -rotate-45 transform rounded-md bg-[#4ffc92]"></div>
         <div className="mb-2 mt-2 h-0.5 w-16 rounded-md bg-[#4ffc92]"></div>
